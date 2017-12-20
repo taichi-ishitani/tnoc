c0e0e90e4c31d1c348ffa13d83f6321caf690f7f
taichi <taichi730@gmail.com>
2017-12-21 00:04:54 +0900

    deleted current implementation

--- sv/env/bfm/compile.f
+++
@@ -1,4 +0,0 @@
-+incdir+${NOC_SV_HOME}/env/bfm
-${NOC_SV_HOME}/env/bfm/noc_bfm_types_pkg.sv
-${NOC_SV_HOME}/env/bfm/noc_bfm_flit_if.sv
-${NOC_SV_HOME}/env/bfm/noc_bfm_pkg.sv
--- sv/env/bfm/noc_bfm_status.svh
+++
@@ -1,4 +0,0 @@
-`ifndef NOC_BFM_STATUS_SVH
-`define NOC_BFM_STATUS_SVH
-typedef tue_status_dummy  noc_bfm_status;
-`endif
