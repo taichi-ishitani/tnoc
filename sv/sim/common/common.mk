a3c58a11171a0bb35bc2b30aedd2f3bd4d022a3d
taichi <taichi730@gmail.com>
2017-12-26 02:07:42 +0900

    added RANDOM_SEED option

--- sv/sim/common/common.mk
+++ sv/sim/common/common.mk
@@ -15,11 +15,12 @@ TEST_LIST	?=
 
 VCS_ARGS	+= -full64 -sverilog -timescale="1ns/1ps" -l vcs.log
 VCS_ARGS	+= -ntb_opts uvm +define+UVM_NO_DEPRECATED +define+UVM_OBJECT_MUST_HAVE_CONSTRUCTO
-SIMV_ARGS	+= +vcs+lic+wait-l $(TEST_NAME).log +UVM_TESTNAME=$(TEST_NAME)
+SIMV_ARGS	+= +vcs+lic+wait -l $(TEST_NAME).log +UVM_TESTNAME=$(TEST_NAME)
 
-DUMP			?= off
-GUI				?= off
-TR_DEBUG	?= off
+DUMP				?= off
+GUI					?= off
+TR_DEBUG		?= off
+RANDOM_SEED	?= auto
 
 ifeq ($(GUI), verdi)
 	VCS_ARGS	+= -lca -debug_access+all -kdb +vcs+fsdbon
@@ -31,6 +32,12 @@ endif
 ifeq ($(GUI), dve)
 	VCS_ARGS	+= +vcs+vcdpluson -debug_access+all
 	SIMV_ARGS	+= -gui=dve
+endif
+
+ifeq ($(RANDOM_SEED), auto)
+	SIMV_ARGS	+= +ntb_random_seed_automatic
+else
+	SIMV_ARGS	+= +ntb_random_seed=$(RANDOM_SEED)
 endif
 
 -include local.mk
