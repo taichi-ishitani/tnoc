f2607079af752bd270967315a9c430992847c282
taichi <taichi730@gmail.com>
2018-02-03 01:21:51 +0900

    changed packet format (close #9)

--- env/bfm/tnoc_bfm_configuration.svh
+++ env/bfm/tnoc_bfm_configuration.svh
@@ -6,12 +6,12 @@ class tnoc_bfm_configuration extends tue_configuration;
   rand  int               byte_enable_width;
   rand  int               id_x_width;
   rand  int               id_y_width;
-  rand  int               vc_width;
   rand  int               id_width;
-  rand  int               tag_width;
-  rand  int               length_width;
-  rand  int               lower_address_width;
   rand  int               virtual_channels;
+  rand  int               vc_width;
+  rand  int               tag_width;
+  rand  int               burst_length_width;
+        int               burst_size_width;
   rand  int               id_x;
   rand  int               id_y;
         tnoc_bfm_flit_vif tx_vif;
@@ -44,26 +44,26 @@ class tnoc_bfm_configuration extends tue_configuration;
     id_width == (id_x_width + id_y_width);
   }
 
+  constraint c_valid_virtual_channels {
+    virtual_channels inside {[1:`TNOC_BFM_MAX_VIRTUAL_CHANNELS]};
+  }
+
   constraint c_valid_vc_width {
-    vc_width inside {[1:`TNOC_BFM_MAX_VC_WIDTH]};
+    solve virtual_channels before vc_width;
+    if (virtual_channels == 1) {
+      vc_width == 1;
+    }
+    else {
+      vc_width == $clog2(virtual_channels);
+    }
   }
 
   constraint c_valid_tag_width {
     tag_width inside {[1:`TNOC_BFM_MAX_TAG_WIDTH]};
   }
 
-  constraint c_valid_length_width {
-    length_width inside {[1:`TNOC_BFM_MAX_LENGTH_WIDTH]};
-  }
-
-  constraint c_default_lower_address {
-    solve data_width before lower_address_width;
-    soft lower_address_width == $clog2(data_width / 8);
-  }
-
-  constraint c_valid_virtual_channels {
-    solve vc_width before virtual_channels;
-    virtual_channels inside {[1:2**vc_width]};
+  constraint c_valid_burst_length {
+    burst_length_width inside {[1:`TNOC_BFM_MAX_BURST_LENGTH_WIDTH]};
   }
 
   constraint c_valid_id {
@@ -73,6 +73,10 @@ class tnoc_bfm_configuration extends tue_configuration;
     id_y inside {[0:(2**id_y_width)-1]};
   }
 
+  function void post_randomize();
+    burst_size_width  = (data_width <= 16) ? 1 : $clog2($clog2(data_width / 8));
+  endfunction
+
   function int get_common_header_width();
     if (common_header_width <= 0) begin
       common_header_width = (
@@ -81,7 +85,6 @@ class tnoc_bfm_configuration extends tue_configuration;
         (id_y_width + id_x_width)    +  //  Source ID
         vc_width                     +  //  Virtual Channel
         tag_width                    +  //  Packet Tag
-        length_width                 +  //  Length
         $bits(tnoc_bfm_routing_mode) +  //  Routing Mode
         1                               //  Invalid Destination Flag
       );
@@ -92,8 +95,11 @@ class tnoc_bfm_configuration extends tue_configuration;
   function int get_request_header_width();
     if (request_header_width <= 0) begin
       request_header_width  = (
-        get_common_header_width() + //  Common Fields
-        address_width               //  Address
+        get_common_header_width()  +  //  Common Fields
+        $bits(tnoc_bfm_burst_type) +  //  burst type
+        burst_length_width         +  //  burst length
+        burst_size_width           +  //  burst size
+        address_width                 //  Address
       );
     end
     return request_header_width;
@@ -102,10 +108,8 @@ class tnoc_bfm_configuration extends tue_configuration;
   function int get_response_header_width();
     if (resposne_header_width <= 0) begin
       resposne_header_width = (
-        get_common_header_width() + //  Common Fields
-        2                         + //  Status
-        lower_address_width       + //  Lower Address
-        1                           //  Last Response Flag
+        get_common_header_width() +     //  Common Fields
+        $bits(tnoc_bfm_response_status) //  status
       );
     end
     return resposne_header_width;
@@ -149,11 +153,11 @@ class tnoc_bfm_configuration extends tue_configuration;
     `uvm_field_int(byte_enable_width  , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(id_x_width         , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(id_y_width         , UVM_DEFAULT | UVM_DEC)
+    `uvm_field_int(virtual_channels   , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(vc_width           , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(tag_width          , UVM_DEFAULT | UVM_DEC)
-    `uvm_field_int(length_width       , UVM_DEFAULT | UVM_DEC)
-    `uvm_field_int(lower_address_width, UVM_DEFAULT | UVM_DEC)
-    `uvm_field_int(virtual_channels   , UVM_DEFAULT | UVM_DEC)
+    `uvm_field_int(burst_length_width , UVM_DEFAULT | UVM_DEC)
+    `uvm_field_int(burst_size_width   , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(id_x               , UVM_DEFAULT | UVM_DEC)
     `uvm_field_int(id_y               , UVM_DEFAULT | UVM_DEC)
   `uvm_object_utils_end
--- rtl/include/tnoc_packet.svh
+++ rtl/include/tnoc_packet.svh
@@ -14,16 +14,28 @@ typedef struct packed {
   tnoc_id_y  y;
 } tnoc_location_id;
 
-typedef logic [CONFIG.vc_width-1:0] tnoc_vc;
+localparam  int VC_WIDTH  = (CONFIG.virtual_channels == 1) ? 1 : $clog2(CONFIG.virtual_channels);
+typedef logic [VC_WIDTH-1:0]  tnoc_vc;
 
 typedef logic [CONFIG.tag_width-1:0]  tnoc_tag;
-
-typedef logic [CONFIG.length_width-1:0] tnoc_length;
 
 typedef enum logic {
   TNOC_X_Y_ROUTING = 'b0,
   TNOC_Y_X_ROUTING = 'b1
 } tnoc_routing_mode;
+
+typedef enum logic [1:0] {
+  TNOC_FIXED_BURST        = 'b00,
+  TNOC_INCREMENTING_BURST = 'b01,
+  TNOC_WRAPPING_BURST     = 'b10
+} tnoc_burst_type;
+
+typedef logic [CONFIG.burst_length_width-1:0] tnoc_burst_length;
+
+localparam  int BURST_SIZE_WIDTH  = (
+  CONFIG.data_width <= 16
+) ? 1 : $clog2($clog2(CONFIG.data_width));
+typedef logic [BURST_SIZE_WIDTH-1:0]  tnoc_burst_size;
 
 typedef logic [CONFIG.address_width-1:0]  tnoc_address;
 
@@ -34,13 +46,9 @@ typedef enum logic [1:0] {
   tnoc_DECODE_ERROR  = 'b11
 } tnoc_response_status;
 
-localparam  int LOWER_ADDRESS_WIDTH = $clog2(CONFIG.data_width / 8);
-typedef logic [LOWER_ADDRESS_WIDTH-1:0] tnoc_lower_address;
-
 `define tnoc_packet_header_common_fields \
 logic             invalid_destination; \
 tnoc_routing_mode routing_mode; \
-tnoc_length       length; \
 tnoc_tag          tag; \
 tnoc_vc           vc; \
 tnoc_location_id  source_id; \
@@ -52,13 +60,14 @@ typedef struct packed {
 } tnoc_common_header;
 
 typedef struct packed {
-  tnoc_address  address;
+  tnoc_address      address;
+  tnoc_burst_size   burst_size;
+  tnoc_burst_length burst_length;
+  tnoc_burst_type   burst_type;
   `tnoc_packet_header_common_fields
 } tnoc_request_header;
 
 typedef struct packed {
-  logic                 last_response;
-  tnoc_lower_address    lower_address;
   tnoc_response_status  status;
   `tnoc_packet_header_common_fields
 } tnoc_response_header;
