fa4290e7100667c3155a2621d9a2c59b4ae96d4b
taichi <taichi730@gmail.com>
2018-11-09 15:59:14 +0900

    removed unnecessary scope

--- rtl/common/tnoc_macros.svh
+++ rtl/common/tnoc_macros.svh
@@ -7,12 +7,10 @@
 tnoc_flit_if #(CONFIG, channel, TNOC_INTERNAL_PORT)
 
 `define tnoc_flit_if_renamer(flit_in_if, flit_out_if) \
-if (1) begin \
-  assign  flit_out_if.valid       = flit_in_if.valid; \
-  assign  flit_in_if.ready        = flit_out_if.ready; \
-  assign  flit_out_if.flit        = flit_in_if.flit; \
-  assign  flit_in_if.vc_available = flit_out_if.vc_available; \
-end
+assign  flit_out_if.valid       = flit_in_if.valid; \
+assign  flit_in_if.ready        = flit_out_if.ready; \
+assign  flit_out_if.flit        = flit_in_if.flit; \
+assign  flit_in_if.vc_available = flit_out_if.vc_available; \
 
 `define tnoc_flit_array_if_renamer(flit_in_if, flit_out_if, size) \
 for (genvar __i = 0;__i < size;++__i) begin \
@@ -23,12 +21,10 @@ for (genvar __i = 0;__i < size;++__i) begin \
 end
 
 `define tnoc_port_control_if_renamer(arbitrator_if, requester_if) \
-if (1) begin \
-  assign  requester_if.request          = arbitrator_if.request; \
-  assign  arbitrator_if.grant           = requester_if.grant; \
-  assign  requester_if.free             = arbitrator_if.free; \
-  assign  requester_if.start_of_packet  = arbitrator_if.start_of_packet; \
-  assign  requester_if.end_of_packet    = arbitrator_if.end_of_packet; \
-end
+assign  requester_if.request          = arbitrator_if.request; \
+assign  arbitrator_if.grant           = requester_if.grant; \
+assign  requester_if.free             = arbitrator_if.free; \
+assign  requester_if.start_of_packet  = arbitrator_if.start_of_packet; \
+assign  requester_if.end_of_packet    = arbitrator_if.end_of_packet; \
 
 `endif
